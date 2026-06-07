a0c14e6a78e03eaee6e3d33f5831f2b1ce2f1fa8
Rollo <@email.com>
2026-06-07 14:27:01 +0300

    fix: import React default for React.Fragment with key prop

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 import Head from "next/head";
 
 const C = {
